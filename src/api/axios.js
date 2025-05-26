129e36a1eb9f16956aa8cd8e11d19dd74bd0d630
mouaadlahmani <mouaadlahmani@gmail.com>
2025-05-27 00:28:28 +0100

    update url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:1337/api/',
+    baseURL: 'https://e-school-api.vercel.app/api/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
