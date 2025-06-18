cb646e69c86d08cbdcced7f6d1a5b64d1b8ebb3d
mouaadlahmani <mouaadlahmani@gmail.com>
2025-06-18 15:42:30 +0100

    update

---
+++ src/pages/Enseignant.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Enseignant = () => {
+    return (
+        <>
+            <Navbar />
+            <div>Enseignant</div>
+        </>
+
+    )
+}
+
+export default Enseignant
